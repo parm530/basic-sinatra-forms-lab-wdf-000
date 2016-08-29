d83cf1ffe8f35975fece4d9bb773a3d819ebddba
parm530 <parmsahadeo530@gmail.com>
2016-08-29 15:03:38 -0400

    Added routes for newteam and team

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/newteam" do
+    erb :newteam
+  end
+
+
+  get "/team" do
+    erb :team
+  end
+
+  post "/team" do
+    puts params
+    erb :team
+  end
+
 
 end
